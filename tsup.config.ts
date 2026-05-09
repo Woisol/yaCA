d85cc42be1ca770e94c51c42b2e45c54f8466ba1
Woisol-G <Woisol-G@outlook.com>
2026-05-09 14:35:37 +0800

    「 :rocket: 构建 / build 」: 发包调整

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,5 +4,8 @@ export default defineConfig({
   entry: ['apps/index.ts'],
   format: ['esm'],
   dts: true,
-  clean: true
+  clean: true,
+  bundle: true,
+  splitting: false,   // 代码分隔，取消以生成单一文件
+  minify: true,
 });
