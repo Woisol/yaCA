d7ae0ba5e2e57e731fe597e4f472873fecbfd547
Woisol-G <Woisol-G@outlook.com>
2026-05-12 10:44:54 +0800

    「 :rocket: 构建 / build 」: WIP 双包实践

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['apps/index.ts'],
+  entry: ['apps/index.ts', 'apps/web-runtime.ts'],
   format: ['esm'],
   dts: true,
   clean: true,
   bundle: true,
+  external: ['@woisol-g/yaca-web', '@woisol-g/yaca-web/server.js', '@woisol-g/yaca-web/server/prompt.js'],
   splitting: false,   // 代码分隔，取消以生成单一文件
   minify: true,
 });
